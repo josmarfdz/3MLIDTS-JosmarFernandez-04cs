60d6927db8ff41a7e70b261ebd5673eb18897631
craft <craft@LAPTOP-0VLEOPG7>
2025-08-28 12:49:43 -0600

    Agregar archivos de proyecto.

---
+++ 3MLIDTS-JosmarFernandez-04cs/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace _3MLIDTS_JosmarFernandez_04cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblEstatura = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtEstatura = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.gpGenero = new System.Windows.Forms.GroupBox();
+            this.rbnMasculino = new System.Windows.Forms.RadioButton();
+            this.rbnFemenino = new System.Windows.Forms.RadioButton();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.gpGenero.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Montserrat SemiBold", 7F);
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(80, 37);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(58, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.BackColor = System.Drawing.Color.Transparent;
+            this.lblApellido.Font = new System.Drawing.Font("Montserrat SemiBold", 7F);
+            this.lblApellido.ForeColor = System.Drawing.Color.White;
+            this.lblApellido.Location = new System.Drawing.Point(80, 68);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(70, 16);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido(s)";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.BackColor = System.Drawing.Color.Transparent;
+            this.lblTelefono.Font = new System.Drawing.Font("Montserrat SemiBold", 7F);
+            this.lblTelefono.ForeColor = System.Drawing.Color.White;
+            this.lblTelefono.Location = new System.Drawing.Point(80, 100);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(58, 16);
+            this.lblTelefono.TabIndex = 2;
+            this.lblTelefono.Text = "Teléfono";
+            // 
+            // lblEstatura
+            // 
+            this.lblEstatura.AutoSize = true;
+            this.lblEstatura.BackColor = System.Drawing.Color.Transparent;
+            this.lblEstatura.Font = new System.Drawing.Font("Montserrat SemiBold", 7F);
+            this.lblEstatura.ForeColor = System.Drawing.Color.White;
+            this.lblEstatura.Location = new System.Drawing.Point(80, 131);
+            this.lblEstatura.Name = "lblEstatura";
+            this.lblEstatura.Size = new System.Drawing.Size(57, 16);
+            this.lblEstatura.TabIndex = 3;
+            this.lblEstatura.Text = "Estatura";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.BackColor = System.Drawing.Color.Transparent;
+            this.lblEdad.Font = new System.Drawing.Font("Montserrat SemiBold", 7F);
+            this.lblEdad.ForeColor = System.Drawing.Color.White;
+            this.lblEdad.Location = new System.Drawing.Point(80, 164);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(38, 16);
+            this.lblEdad.TabIndex = 4;
+            this.lblEdad.Text = "Edad";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.txtNombre.Location = new System.Drawing.Point(154, 37);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(147, 24);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.txtApellido.Location = new System.Drawing.Point(154, 68);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(147, 24);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.txtTelefono.Location = new System.Drawing.Point(154, 100);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(147, 24);
+            this.txtTelefono.TabIndex = 7;
+            // 
+            // txtEstatura
+            // 
+            this.txtEstatura.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.txtEstatura.Location = new System.Drawing.Point(154, 131);
+            this.txtEstatura.Name = "txtEstatura";
+            this.txtEstatura.Size = new System.Drawing.Size(147, 24);
+            this.txtEstatura.TabIndex = 8;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.txtEdad.Location = new System.Drawing.Point(154, 164);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(147, 24);
+            this.txtEdad.TabIndex = 9;
+            // 
+            // gpGenero
+            // 
+            this.gpGenero.BackColor = System.Drawing.Color.Transparent;
+            this.gpGenero.Controls.Add(this.rbnMasculino);
+            this.gpGenero.Controls.Add(this.rbnFemenino);
+            this.gpGenero.Font = new System.Drawing.Font("Montserrat SemiBold", 8.25F);
+            this.gpGenero.ForeColor = System.Drawing.Color.White;
+            this.gpGenero.Location = new System.Drawing.Point(83, 208);
+            this.gpGenero.Name = "gpGenero";
+            this.gpGenero.Size = new System.Drawing.Size(218, 74);
+            this.gpGenero.TabIndex = 10;
+            this.gpGenero.TabStop = false;
+            this.gpGenero.Text = "Género";
+            // 
+            // rbnMasculino
+            // 
+            this.rbnMasculino.AutoSize = true;
+            this.rbnMasculino.Location = new System.Drawing.Point(6, 23);
+            this.rbnMasculino.Name = "rbnMasculino";
+            this.rbnMasculino.Size = new System.Drawing.Size(104, 24);
+            this.rbnMasculino.TabIndex = 0;
+            this.rbnMasculino.TabStop = true;
+            this.rbnMasculino.Text = "Masculino";
+            this.rbnMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbnFemenino
+            // 
+            this.rbnFemenino.AutoSize = true;
+            this.rbnFemenino.Location = new System.Drawing.Point(110, 23);
+            this.rbnFemenino.Name = "rbnFemenino";
+            this.rbnFemenino.Size = new System.Drawing.Size(105, 24);
+            this.rbnFemenino.TabIndex = 1;
+            this.rbnFemenino.TabStop = true;
+            this.rbnFemenino.Text = "Femenino";
+            this.rbnFemenino.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnGuardar.Font = new System.Drawing.Font("Montserrat SemiBold", 8.25F);
+            this.btnGuardar.ForeColor = System.Drawing.Color.Green;
+            this.btnGuardar.Location = new System.Drawing.Point(83, 288);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(97, 34);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnBorrar.Font = new System.Drawing.Font("Montserrat SemiBold", 8.25F);
+            this.btnBorrar.ForeColor = System.Drawing.Color.Maroon;
+            this.btnBorrar.Location = new System.Drawing.Point(203, 288);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(98, 34);
+            this.btnBorrar.TabIndex = 12;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.gpGenero);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtEstatura);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblEstatura);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Formulario de Registro";
+            this.gpGenero.ResumeLayout(false);
+            this.gpGenero.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblEstatura;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtEstatura;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.GroupBox gpGenero;
+        private System.Windows.Forms.RadioButton rbnMasculino;
+        private System.Windows.Forms.RadioButton rbnFemenino;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
