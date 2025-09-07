8462e4828321196baf9fab5834f74388ebe46479
Josmar Fernández <josmar.fernandez58@unach.mx>
2025-09-06 21:25:35 -0600

    Update Form1.cs

--- 3MLIDTS-JosmarFernandez-04cs/Form1.cs
+++ 3MLIDTS-JosmarFernandez-04cs/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions; //Librería para comparar formatos de texto
 
 namespace _3MLIDTS_JosmarFernandez_04cs
 {
@@ -15,6 +17,97 @@ namespace _3MLIDTS_JosmarFernandez_04cs
         public Form1()
         {
             InitializeComponent();
+            txtNombre.TextChanged += validarNombre;
+            txtEdad.TextChanged += validarEdad;
+            txtEstatura.TextChanged += validarEstatura;
+            txtApellido.TextChanged += validarApellido;
+            txtTelefono.TextChanged += validarTelefono;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtEdad.Clear();
+            txtEstatura.Clear();
+            txtTelefono.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            rbnMasculino.Checked = false;
+            rbnFemenino.Checked = false;
+        }
+
+        private void validarNombre(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if(!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Ingrese valores válidos para el nombre", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool EsTextoValido(string texto)
+        {
+            return Regex.IsMatch(texto,@"^[a-zA-Z\s]+$");
+        }
+
+        private void validarEdad(object sender, EventArgs e)
+        {
+
+        }
+
+        private void validarEstatura(object sender, EventArgs e)
+        {
+
+        }
+
+        private void validarApellido(object sender, EventArgs e)
+        {
+
+        }
+
+        private void validarTelefono(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string name = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string est = txtEstatura.Text;
+            string num = txtTelefono.Text;
+            string edad = txtEdad.Text;
+            string genero = "";
+            if (rbnMasculino.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (rbnFemenino.Checked)
+            {
+
+                genero = "Mujer";
+            }
+            if (!string.IsNullOrEmpty(txtEdad.Text) || !string.IsNullOrEmpty(txtEstatura.Text) || !string.IsNullOrEmpty(txtNombre.Text) || !string.IsNullOrEmpty(txtTelefono.Text) || !string.IsNullOrEmpty(txtApellido.Text))
+            {
+                String datos = $"Nombre:{name}\n\rApellido:{apellido}\n\r Edad:{edad} \n\r Altura: {est} \n\r Telefono: {num} \n\r Sex: {genero}";
+                string ruta = @"C:\Users\craft\Documents\Códigos\txt\Datos3MAgosto2025.txt";
+                bool archivoExt = File.Exists(ruta);
+                using (StreamWriter writer = new StreamWriter(ruta, true))
+                {
+                    if (archivoExt)
+                    {
+                        writer.WriteLine();
+                    }
+                    writer.WriteLine(datos);
+                }
+                MessageBox.Show(datos, "Informacion de registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+
+                // String datos = $"Nombre:{name}\n\rAPellido:{sur}\n\r Edad:{age} \n\r Altura: {altura} \n\r Telefono: {cel} \n\r Sex: {genero}";
+                MessageBox.Show("ingrese valores a los espacios en blanco", "error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
         }
     }
 }
